040269857e505b9e2a511880eb7591110b9c2040
olive <olive@DESKTOP-R9E0C6C.home>
2022-03-24 16:30:30 +0100

    Added player and main class with small details

---
+++ Game-v1/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("########################################");
+		System.out.println("#      Welcome to the game player!     #");
+		System.out.println("########################################");
+		System.out.println();
+	}
+}
--- Game-v1/src/main/Player.java
+++ Game-v1/src/main/Player.java
@@ -1,5 +1,39 @@
 package main;
 
 public class Player {
+	String name;
+	double health;
+	double damage;
+	
+	public Player(String name) {
+		this.name = name;
+		health = 100;
+		damage = 10;
+	}
 
+	//Getters
+	public String getName() {
+		return name;
+	}
+
+	public double getHealth() {
+		return health;
+	}
+
+	public double getDamage() {
+		return damage;
+	}
+
+	//Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setHealth(double health) {
+		this.health = health;
+	}
+
+	public void setDamage(double damage) {
+		this.damage = damage;
+	}
 }
