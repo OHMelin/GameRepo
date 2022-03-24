b929526dad89a2299379920b0131fe28a53e7bfe
olive <olive@DESKTOP-R9E0C6C.home>
2022-03-24 16:48:06 +0100

    Health class

--- Game-v1/src/main/Player.java
+++ Game-v1/src/main/Player.java
@@ -2,26 +2,16 @@ package main;
 
 public class Player {
 	String name;
-	double health;
-	double currentHealth;
 	double damage;
 	
 	public Player(String name) {
 		this.name = name;
-		health = 100;
-		currentHealth = 100;
 		damage = 10;
 	}
 
 	//Getters
 	public String getName() {
 		return name;
-	}
-	public double getHealth() {
-		return health;
-	}
-	public double getCurrentHealth() {
-		return currentHealth;
 	}
 	public double getDamage() {
 		return damage;
@@ -30,12 +20,6 @@ public class Player {
 	//Setters
 	public void setName(String name) {
 		this.name = name;
-	}
-	public void setHealth(double health) {
-		this.health = health;
-	}
-	public void setCurrentHealth(double currentHealth) {
-		this.currentHealth = currentHealth;
 	}
 	public void setDamage(double damage) {
 		this.damage = damage;
