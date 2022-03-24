68e8cd57b0be6733f04bb637f33e467911e2cfc5
olive <olive@DESKTOP-R9E0C6C.home>
2022-03-24 17:25:16 +0100

    - Added health/exp bar comments - Added "adders" for health and exp

---
+++ Game-v1/src/main/Experience.java
@@ -0,0 +1,71 @@
+package main;
+
+public class Experience {
+	double currentExp;
+	int currentLevel;
+	double expToLevel;
+	
+	public Experience() {
+		currentExp = 0;
+		currentLevel = 1;
+		expToLevel = 100+currentLevel*2;
+	}
+	
+	//Getters
+	public double getCurrentExp() {
+		return currentExp;
+	}
+	public int getCurrentLevel() {
+		return currentLevel;
+	}
+	public double getExpToLevel() {
+		return expToLevel;
+	}
+	
+	//Setters
+	public void setCurrentExp(double currentExp) {
+		this.currentExp = currentExp;
+	}
+	public void setCurrentLevel(int currentLevel) {
+		this.currentLevel = currentLevel;
+	}
+	public void setExpToLevel(double expToLevel) {
+		this.expToLevel = expToLevel;
+	}
+	
+	//Adders
+	public void addExp(double amount) {
+		currentExp += amount;
+		if(currentExp >= expToLevel) {
+			addLvl();
+		}
+	}
+	public void addLvl() {
+		currentExp -= expToLevel;
+		currentLevel++;
+	}
+
+	//Display exp bar
+	/*
+	 * String full is the full string we wish to return. I start by adding a start, then for every time i < currentExp/10 i print |
+	 * If current exp is 60 it will print 6 characters
+	 * My j value starts at 10 as the health bar length is 10 characters. So for every time we print | i subtract 1
+	 * Then I print . for every j left. Which is 4 (10-6)
+	 * At last I print the end of the string and return the full string
+	 */
+	public String displayExpBar() {		
+		String full = "[ ";
+		int j = 10;
+		
+		for(int i = 0; i < currentExp/10; i++) {
+			full += "|";
+			j--;
+		}
+		for(int i = 0; i < j; i++) {
+			full += ".";
+		}
+		full += " ]";
+		
+		return full;
+	}	
+}
