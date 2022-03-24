96aee1c36858d3abd64c1eee1bf1889d84ecdb55
olive <olive@DESKTOP-R9E0C6C.home>
2022-03-24 16:42:25 +0100

    Added currentHealth

--- Game-v1/src/main/Player.java
+++ Game-v1/src/main/Player.java
@@ -3,11 +3,13 @@ package main;
 public class Player {
 	String name;
 	double health;
+	double currentHealth;
 	double damage;
 	
 	public Player(String name) {
 		this.name = name;
 		health = 100;
+		currentHealth = 100;
 		damage = 10;
 	}
 
@@ -15,11 +17,12 @@ public class Player {
 	public String getName() {
 		return name;
 	}
-
 	public double getHealth() {
 		return health;
 	}
-
+	public double getCurrentHealth() {
+		return currentHealth;
+	}
 	public double getDamage() {
 		return damage;
 	}
@@ -28,11 +31,12 @@ public class Player {
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public void setHealth(double health) {
 		this.health = health;
 	}
-
+	public void setCurrentHealth(double currentHealth) {
+		this.currentHealth = currentHealth;
+	}
 	public void setDamage(double damage) {
 		this.damage = damage;
 	}
