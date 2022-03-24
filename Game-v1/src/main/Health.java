a7687b2f4e6b475d761cfaa283cf0e5052303259
olive <olive@DESKTOP-R9E0C6C.home>
2022-03-24 16:48:34 +0100

    Health fixed

---
+++ Game-v1/src/main/Health.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Health {
+	double maxHealth;
+	double currentHealth;
+	
+	//Getters
+	public double getMaxHealth() {
+		return maxHealth;
+	}
+	public double getCurrentHealth() {
+		return currentHealth;
+	}
+	
+	//Setters
+	public void setHealth(double maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+	public void setCurrentHealth(double currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+}
